2891e28d91657d6c48b5c82d784e0832d42e1b32
Bengü Özcan <bengu.ozcan@bil.omu.edu.tr>
2015-08-18 04:57:34 +0300

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def edit_profile
+
+  end
+
+  def update_profile
+  	@user = current_user.update(user_params)
+  	redirect_to root_path
+  end
+
+  private
+  	def user_params
+		params.require(:user).permit(:avatar)
+  	end
+
+end
---
+++ db/migrate/20150817215716_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+    	t.text :content
+    	t.integer :user_id
+    	t.integer :category_id
+
+
+      t.timestamps null: false
+    end
+  end
+end
